1caec7b1123bb4211db725a60c2b3d5c1ed21b35
Gabriel Bonaretti <114812017+GabrielBonaretti@users.noreply.github.com>
2023-06-05 21:49:39 -0300

    Update extrair.py

--- modules/extrair.py
+++ modules/extrair.py
@@ -12,38 +12,28 @@ class Extrair:
         }
 
     def criar_excel(self):
-        # data
         df = pd.DataFrame(self.celulares)
-
-        # create writer object
+        
         writer = pd.ExcelWriter('./files/Table.xlsx', engine='xlsxwriter')
 
-        # add data to the sheet
         df.to_excel(writer, sheet_name="Sheet1", index=False)
 
-        # get workbook
         workbook = writer.book
 
-        # get sheet for conditional formatting
         worksheet = writer.sheets['Sheet1']
 
-        # create chart object
         chart = workbook.add_chart({'type': 'column'})
 
         print(str(len(self.celulares["Preço"]) + 1))
 
-        # configure the series of the chart from the dataframe data
         chart.add_series({'categories': f'=Sheet1!$B$2:$B${str(len(self.celulares["Preço"]) + 1)}',
                           'values': f'=Sheet1!$D$2:$D${str(len(self.celulares["Preço"]) + 1)}'})
 
-        # insert chart into the worksheet
         worksheet.insert_chart('G1', chart)
 
-        # close file
         writer.close()
 
     def criar_csv(self):
         df = pd.DataFrame(self.celulares)
-
-        # Write DataFrame to CSV File with Default params.
+        
         df.to_csv("./files/file.csv", header=False)
